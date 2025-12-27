65b1b6b8f2b630d44ea845b71ddcdc41f53238c4
DanilRozhin <danilrozhin2005@mail.ru>
2025-12-27 15:15:12 +0300

    Completed user model

--- shop_app/database/models/user.py
+++ shop_app/database/models/user.py
@@ -1,16 +1,34 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 
+import uuid
 from shop_app.database.database import Base
 
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = 'user'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str]
-    hashed_password: Mapped[str]
-    name: Mapped[str] = mapped_column(default='Anonymous')
-    email: Mapped[str | None]
-    phone: Mapped[str | None]
-    is_blocked: Mapped[bool] = mapped_column(default=False)
-    is_deleted: Mapped[bool] = mapped_column(default=False)
+    id: Mapped[uuid.UUID] = mapped_column(
+        PG_UUID(as_uuid=True),
+        primary_key=True,
+        server_default=text('gen_random_uuid()'),
+    )
+    username: Mapped[str] = mapped_column(nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(default='Anonymous', nullable=False)
+    email: Mapped[str | None] = mapped_column(nullable=True)
+    phone: Mapped[str | None] = mapped_column(nullable=True)
+    is_blocked: Mapped[bool] = mapped_column(default=False, nullable=False)
+    is_deleted: Mapped[bool] = mapped_column(default=False, nullable=False)
+
+    reviews: Mapped[list['Review']] = relationship(
+        back_populates='review',
+    )
+    orders: Mapped[list['Order']] = relationship(
+        back_populates='order',
+    )
+    promocodes: Mapped[list['Promocode']] = relationship(
+        back_populates='promocode',
+        secondary='user_promocode_assignment',
+    )
