b408909a0b48e75d11dba130699af515dad8bc33
DanilRozhin <danilrozhin2005@mail.ru>
2025-12-27 14:59:45 +0300

    Completed review model

--- shop_app/database/models/review.py
+++ shop_app/database/models/review.py
@@ -1,16 +1,42 @@
-from sqlalchemy import ForeignKey, SmallInteger
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, SmallInteger, text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 
+import uuid
 from shop_app.database.database import Base
 
 
 class Review(Base):
     __tablename__ = 'review'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int | None] = mapped_column(ForeignKey('user.id', ondelete='CASCADE'))
-    parameter_id: Mapped[int]
-    start: Mapped[int] = mapped_column(SmallInteger)
-    comment: Mapped[str | None]
+    id: Mapped[uuid.UUID] = mapped_column(
+        PG_UUID(as_uuid=True),
+        primary_key=True,
+        server_default=text('gen_random_uuid()'),
+    )
+    user_id: Mapped[int | None] = mapped_column(
+        PG_UUID(as_uuid=True),
+        ForeignKey(
+            'user.id',
+            name='review_user_id_fk',
+            ondelete='SET NULL',),
+        nullable=True,
+    )
+    parameter_id: Mapped[uuid.UUID] = mapped_column(
+        PG_UUID(as_uuid=True),
+        ForeignKey(
+            'parameter.id',
+            name='review_parameter_id_fk',
+            ondelete='CASCADE',
+        ),
+        nullable=False,
+    )
+    start: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    comment: Mapped[str | None] = mapped_column(nullable=True)
 
-# Что делать с параметром и валидация звезд от 1 до 5
+    product_parameter: Mapped['ProductParameter'] = relationship(
+        back_populates='product_parameter',
+    )
+    user: Mapped['User'] = relationship(
+        back_populates='user',
+    )
