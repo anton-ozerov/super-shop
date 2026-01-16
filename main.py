08bfde95311332e629c59e618d7ea527a7df2dbe
DanilRozhin <danilrozhin2005@mail.ru>
2026-01-16 15:32:02 +0300

    Included categories router

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 
 from shop_app.logging.config import setup_logging
 from shop_app.middleware import access_log_middleware, request_id_middleware
-from shop_app.routers import health_router
+from shop_app.routers import categories_router, health_router
 
 setup_logging()
 logger = logging.getLogger(__name__)
@@ -18,6 +18,7 @@ app.middleware("http")(request_id_middleware)
 
 # routers
 app.include_router(health_router)
+app.include_router(categories_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
