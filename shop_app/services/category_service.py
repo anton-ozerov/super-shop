1784d336a95750f17a08b05cabedee69234c93ea
Anton Ozerov <ozerovanton17@gmail.com>
2026-01-16 13:22:37 +0300

    remade work with service

--- shop_app/services/category_service.py
+++ shop_app/services/category_service.py
@@ -1,11 +1,15 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from shop_app.repositories import CategoryRepository
 from shop_app.schemas import CategoriesAll
 
 
 class CategoryService:
-    @classmethod
-    async def get_all_categories(cls, cat_repo: CategoryRepository) -> CategoriesAll:
-        cats = await cat_repo.get_all_categories()
+    def __init__(self, session: AsyncSession):
+        self.cat_repo: CategoryRepository = CategoryRepository(session=session)
+
+    async def get_all_categories(self) -> CategoriesAll:
+        cats = await self.cat_repo.get_all_categories()
         if cats is None:
             return CategoriesAll(status=True, message="No categories found", categories=[], total_count=0)
         return CategoriesAll(
