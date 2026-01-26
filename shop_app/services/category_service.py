a9ffa3ad095298ea7fa47a31858e3a4065961c86
DanilRozhin <danilrozhin2005@mail.ru>
2026-01-26 14:21:09 +0300

    Added get pagination call to categories service

--- shop_app/services/category_service.py
+++ shop_app/services/category_service.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from shop_app.repositories import CategoryRepository
 from shop_app.schemas import CategoriesAll
+from shop_app.services.pagination_service import get_pagination
 
 
 class CategoryService:
@@ -9,7 +10,17 @@ class CategoryService:
         self.cat_repo: CategoryRepository = CategoryRepository(session=session)
 
     async def get_all_categories(self, page, per_page) -> CategoriesAll:
-        cats, pagination = await self.cat_repo.get_all_categories(page, per_page)
+        cats, total_cats = await self.cat_repo.get_all_categories(page=page, per_page=per_page)
+        if cats:
+            current_items = len(cats)
+        else:
+            current_items = 0
+        pagination = get_pagination(
+            total_items=total_cats,
+            page=page,
+            per_page=per_page,
+            current_items=current_items,
+        )
         if cats is None:
             return CategoriesAll(
                 status=True,
