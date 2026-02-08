ea2f5e33b17bec5b1578c2916b95b1c94de42ab1
DanilRozhin <danilrozhin2005@mail.ru>
2026-02-08 17:33:14 +0300

    Added test engine, sessions, overriding dependencies to conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,64 @@
 import os
 
-import pytest
-from dotenv import load_dotenv
+import pytest_asyncio
 from httpx import ASGITransport, AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-load_dotenv(".test.env", override=True)
+from main import create_app
+from shop_app.database.database import Base, get_async_session
 
-assert str(os.getenv("ENVIRONMENT")) == "TEST"
+TEST_DB_NAME = str(os.getenv("TEST_DB_NAME"))
+TEST_DB_USER = str(os.getenv("TEST_DB_USER"))
+TEST_DB_PASSWORD = str(os.getenv("TEST_DB_PASSWORD"))
+TEST_DB_HOST = str(os.getenv("TEST_DB_HOST"))
+TEST_DB_PORT = str(os.getenv("TEST_DB_PORT"))
+TEST_DATABASE_URL = (
+    f"postgresql+asyncpg://{TEST_DB_USER}:{TEST_DB_PASSWORD}@{TEST_DB_HOST}:{TEST_DB_PORT}/{TEST_DB_NAME}"
+)
 
-from main import create_app  # noqa
+
+@pytest_asyncio.fixture(scope="function")
+async def test_engine():
+    engine = create_async_engine(TEST_DATABASE_URL, echo=False)
+
+    try:
+        yield engine
+    finally:
+        await engine.dispose()
 
 
-@pytest.fixture(scope="session")
-def app():
-    return create_app()
+@pytest_asyncio.fixture(scope="function")
+def session_maker(test_engine):
+    return async_sessionmaker(bind=test_engine, class_=AsyncSession, expire_on_commit=False)
 
 
-@pytest.fixture
-async def async_client(app):
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-        yield ac
+@pytest_asyncio.fixture(scope="function")
+async def db_session(test_engine, session_maker):
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    async with session_maker() as session:
+        yield session
+
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest_asyncio.fixture
+async def async_client(db_session, session_maker):
+    app = create_app()
+
+    async def override_get_async_session():
+        async with session_maker() as session:
+            yield session
+
+    app.dependency_overrides[get_async_session] = override_get_async_session
+
+    try:
+        async with AsyncClient(
+            transport=ASGITransport(app=app),
+            base_url="http://test",
+        ) as ac:
+            yield ac
+    finally:
+        app.dependency_overrides.clear()
